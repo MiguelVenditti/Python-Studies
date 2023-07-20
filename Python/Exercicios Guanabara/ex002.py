4c0c56230438ab0bc77420512b9d7f223c937aa5
MiguelVenditti <miguis.venditti@gmail.com>
2023-07-19 22:30:24 -0300

    Adicionando os primeiros exercicios, finalmente

---
+++ Python/Exercicios Guanabara/ex002.py
@@ -0,0 +1,6 @@
+x = int(input("Por favor, digite um numero!\nR: "))
+y = int(input("Agora digite um numero para ser somado ao anterior!\nR: "))
+
+soma = x + y
+
+print("A soma de {} + {} é igual a {}".format(x, y, soma))
